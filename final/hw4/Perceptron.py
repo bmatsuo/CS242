6acfe7ad43f82a9e18bbaa705374e8356fc435cf
Bryan Matsuo <bryan.matsuo@gmail.com>
2011-04-28 01:50:12 -0700

    Add stuff to Perception.py for experiment 1.

--- final/hw4/Perceptron.py
+++ final/hw4/Perceptron.py
@@ -30,6 +30,7 @@ class Perceptron:
         """
         (w1,w2) = self.w[self.i]
         lpred = cmp((w1*x1+w2*x2),0) #returns -1 or 1
+        if lpred == 0: lpred = -1
         if lpred != label: #need to update weights
             w1 = w1 + (self.lr * label * x1)
             w2 = w2 + (self.lr * label * x2)
@@ -101,72 +102,108 @@ class Perceptron:
 if __name__ == '__main__':
     import sys
     import random
-    
-    
-    if len(sys.argv) < 4:
-        sys.stderr.write("\nUsage:%s REQUIRED: train.txt test.txt train.log OPTIONAL:[# training iterations, default: 1] [Test Function(one of:'last','longest','voted','last50') default: 'last'] >output.txt\n\n"%(sys.argv[0]))
-        sys.exit(1)
-    
-    train = open(sys.argv[1],'r')
-    test = open(sys.argv[2],'r')
-    trainLog = open(sys.argv[3],'w')
-    
-    perceptron = Perceptron()
-    
-    #defaults:
-    testFunction = perceptron.lastHypothesisClassify
-    trainItter = 1
-    
-    if len(sys.argv) > 4:
-        trainItter = int(sys.argv[4])
-        
-    if len(sys.argv) > 5:
-        arg = sys.argv[3]
-        if arg == 'last':
-            testFunction = perceptron.lastHypothesisClassify
-        elif arg == 'longest':
-            testFunction = perceptron.longestSurvivorClassify
-        elif arg == 'voted':
-            testFunction = perceptron.votedHypothesisClassify
-        elif arg == 'last50':
-            testFunction = perceptron.last50VotesClassify
-        else:
-            sys.stderr.write("\nError: Do not recognize test function: %s, run again with no args for help.\n\n"%arg)
-            sys.exit(1)
+    from optparse import OptionParser, make_option
 
-    trainData = []
-    testData = []
+    options = [
+        make_option("--no-shuffle", "-s", action="store_false", default=True, dest="should_shuffle",
+            help="Don't shuffle data between successive training loops."),
+        make_option("--log", "-L", dest="log_file", default='pa.log',
+            help="Log filename (default: 'pa.log')"),
+        make_option("--rule", '-r', dest="rule", default='last',
+            help="Prediction rule ('last' (default),'longest','voted','last50')."),
+        make_option('--consistent', '-c', action='store_true', dest='want_consistent',
+            help='Loop over the training set until a consistent weight vector is found'),
+        make_option("--iterations", "-T", type='int', dest='iterations', default=1,
+            help="Number of training iterations (default: 1).")]
+    p = OptionParser(
+        description="Train perceptron algorithm and verify against a test set.",
+        usage='Perception.py [options] TRAIN TEST',
+        option_list=options,)
+    (opt, args) = p.parse_args()
     
-    for line in train:
+    if len(args) < 1:
+        sys.stderr.write("%s\nUse the -h option for more info." % p.usage)
+        sys.exit(1)
+
+    perceptron = Perceptron()
+
+    rule_named = {
+            'last':perceptron.lastHypothesisClassify,
+            'longest':perceptron.longestSurvivorClassify,
+            'voted':perceptron.votedHypothesisClassify,
+            'last50':perceptron.last50VotesClassify,}
+
+    if not opt.rule.lower() in rule_named:
+        raise Exception("Unrecognized rule %s" % opt.rule)
+
+    testFunction = rule_named[opt.rule.lower()]
+    trainItter = opt.iterations
+
+    def _parse_line(line):
         #each line should be "x_1    x_2    class"
         #where x_n is a float and class is either +1(or 1) or -1
         #(python can handle unary +)
         line = line.split()
         if line:
-            x1 = float(line[0])
-            x2 = float(line[1])
-            label = int(line[2])
-            trainData.append((x1,x2,label))
+            return (float(line[0]), float(line[1]), int(line[2]))
+        return
+
+    trainData = []
+    train = open(args[1],'r')
+    for line in train:
+        example = _parse_line(line)
+        if not example is None: trainData.append(example)
     train.close()
+
+    testData = []
+    test = open(args[1],'r')
     for line in test:
-        line = line.split()
-        if line:
-            x1 = float(line[0])
-            x2 = float(line[1])
-            label = int(line[2])
-            testData.append((x1,x2,label))
+        example = _parse_line(line)
+        if not example is None: testData.append(example)
     test.close()
     
     #train the perceptron on the set of data
+    trainLog = open(opt.log_file, 'w')
     trainLog.write("#x1\tx2\tlabel\tpred\n")
-    for i in range(trainItter):
-        if i > 0: #shuffle after first iteration through the data
-            random.shuffle(trainData)
-        trainLog.write("")
-        for (x1,x2,label) in trainData:
-            trainLog.write("%.3f\t.3f\t%d\t%d\n"%(x1,x2,label,perceptron.train(x1, x2, label)))
+    mistakes = 0
+    made_mistake = False
+    i = 0
+    if not opt.want_consistent:
+        for i in range(trainItter):
+            made_mistake = False
+            if opt.should_shuffle and i > 0: #shuffle after first iteration through the data
+                random.shuffle(trainData)
+            for (x1,x2,label) in trainData:
+                pred = perceptron.train(x1, x2, label)
+                if label != pred:
+                    mistakes += 1
+                    made_mistake = True
+                trainLog.write("%.3f\t%.3f\t%d\t%d\n" % (x1,x2,label,pred))
+            if not made_mistake:
+                break
+    else:
+        while(1):
+            made_mistake = False
+            if opt.should_shuffle and i > 1: #shuffle after first iteration through the data
+                random.shuffle(trainData)
+            for (x1,x2,label) in trainData:
+                pred = perceptron.train(x1, x2, label)
+                if label != pred:
+                    mistakes += 1
+                    made_mistake = True
+                trainLog.write("%.3f\t%.3f\t%d\t%d\n" % (x1,x2,label,pred))
+            if not made_mistake:
+                break
+            i += 1
+
+    print("TRAINING SUMMARY:")
+    print("Iterations: %d." % (i + 1))
+    print("Total mistakes: %d" % mistakes)
+    print("Total predictions: %d" % ((i + 1)*len(trainData)))
+    print("Consistent hypothesis: %s" % ('YES' if not made_mistake else 'NO'))
+            
     
     #run tests, to standard out write the following
     print("#x1\tx2\tlabel\tpred")
     for (x1,x2,label) in testData:
-        print("%.3f\t%.3f\t%d\t%d"%(x1,x2,label,testFunction(x1,x2)))
+        print("%.3f\t%.3f\t%d\t%d" % (x1,x2,label,testFunction(x1,x2)))
